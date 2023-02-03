fdaa1aa82e9a99c153f7d02d019360bafa5a79e2
duraznodev <romanrizodev@gmail.com>
2023-02-03 12:16:31 -0600

    change title, add emailjs and add animate-bounce-slow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        "bounce-slow": "bounce 1.8s linear infinite",
+      },
     },
   },
   plugins: [],
